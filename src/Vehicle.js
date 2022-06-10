4d3b2a419e0411b4271f9a75aa292323578edf2d
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-10 11:46:44 -0700

    building out fx

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,9 +1,22 @@
-export default function Vehicle() {
+export default function Vehicle(props) {
   return <div className="vehicle">
     {/* the Vehcile component has a prop called vehicle. Check to see what string is in this vehicle prop */}
+    <p>
+      {props.Vehicle === 'car' && '🚗'}
+    </p>
     {/* if the vehicle props is a car, render a '🚗' emoji */}
+    <p>
+      {props.Vehicle === 'truck' && '🚚'}
+    </p>
     {/* if the vehicle props is a truck, render a '🚚' emoji */}
+    <p>
+      {props.Vehicle === 'bus' && '🚌'}
+    </p>
     {/* if the vehicle props is a bus, render a '🚌' emoji */}
+    <p>
+      {props.Vehicle === 'motorcycle' && '🏍️'}
+    </p>
     {/* if the vehicle props is a motorcycle, render a '🏍️' emoji */}
+  
   </div>;
 }
--- src/VehicleList.js
+++ src/VehicleList.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Vehicle from './Vehicle.js';
 
-export default function VehicleList() {
+export default function VehicleList(props) {
   return (
     <div className="traffic">
-      {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
-      {/* map through the vehicles array in props */}
-      {/* for each item render a Vehicle component. Pass the vehicl string as a prop called vehicle to the Vehicle component.  */}
+      {props.Vehicle.map(vehicle, i) => <Vehicle key={vehicle + i} vehicle={vehicle} />
+      
     </div>
   );
 }
